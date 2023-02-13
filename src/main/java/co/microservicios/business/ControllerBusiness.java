7bdf2ae9ad0105ce887e14f426ea8c7d733972c8
Oscar Ivan Estrada <oiestradag@gmail.com>
2023-02-12 21:54:58 -0500

    set viewed

--- src/main/java/co/microservicios/business/ControllerBusiness.java
+++ src/main/java/co/microservicios/business/ControllerBusiness.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import co.microservicios.model.Job;
+import co.microservicios.model.Trigger;
 import co.microservicios.repository.JobRepository;
+import co.microservicios.repository.TriggerRepository;
 import co.microservicios.util.LoggerUtil;
 
 
@@ -17,9 +19,22 @@ public class ControllerBusiness {
 	@Autowired
 	JobRepository jobRepository;
 
+	@Autowired
+	TriggerRepository tRepository;
+
+
 
 	public Job findPendingJobByGroupAndJobName( String groupName, String jobName){
-		return jobRepository.findPendingJobByGroupAndJobName(groupName, jobName);
+
+		Job job = jobRepository.findPendingJobByGroupAndJobName(groupName, jobName); 
+		if(job != null){
+			for(Trigger t: job.getTriggers()){
+				t.setViewed(true);
+				tRepository.save(t);
+			}
+		}
+		
+		return job;
 	}
 
 
